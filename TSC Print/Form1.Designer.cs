11b9dd39839f04b935ecf987ead505ffa12f6a3f
Elia Haider <baqir.ali2012@gmail.com>
2021-12-04 12:27:32 +0500

    Updated

--- TSC Print/Form1.Designer.cs
+++ TSC Print/Form1.Designer.cs
@@ -129,6 +129,7 @@ namespace TSC_Print
             this.button1.TabIndex = 2;
             this.button1.Text = "Print";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // panel1
             // 
--- TSC Print/Form1.cs
+++ TSC Print/Form1.cs
@@ -21,5 +21,31 @@ namespace TSC_Print
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string bName = txtBusinessName.Text;
+                string pName = txtProductName.Text;
+                string pCode = txtProductCode.Text;
+                double price = double.Parse(txtProductPrice.Text);
+
+                    TSCLIB_DLL.openport("Printer Name");         //Open specified printer driver
+                    TSCLIB_DLL.setup("35", "25", "4", "8", "0", "2.5", "0");    //Setup the media size and sensor type info
+                    TSCLIB_DLL.clearbuffer();     //Clear image buffer
+                    TSCLIB_DLL.windowsfont(18, 25, 25, 0, 2, 0, "Century Gothic", bName);  //Draw Business Name
+                    TSCLIB_DLL.barcode("18", "55", "128", "60", "1", "0", "2", "2", pCode); //Drawing barcode
+                    TSCLIB_DLL.windowsfont(18, 135, 30, 0, 2, 0, "Century Gothic", pName);  //Draw Product Name
+                    TSCLIB_DLL.windowsfont(18, 165, 30, 0, 2, 0, "Century Gothic", "Retail: Rs." + price);  //Draw Price
+                    TSCLIB_DLL.sendcommand("PUTPCX 100,400,\"UL.PCX\"");  //Drawing PCX graphic
+                    TSCLIB_DLL.printlabel("1", "1"); //Print labels
+                    TSCLIB_DLL.closeport();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
     }
 }
